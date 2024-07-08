5d5c96f9cda655ac8841a2ba3277f7456ece60be
LucasMantvani <joaolucasmbaioco@gmail.com>
2024-07-08 09:09:48 -0300

    #

--- classes/classes.py
+++ classes/classes.py
@@ -1,7 +1,39 @@
 import requests 
 import pandas as pd
+import numpy as np
 
-import time
+
+'''Para uma abordagem mais precisa para a identificação de domínios conservados, você pode usar a API do NCBI CDD:'''
+
+
+todas_hierarquias = {'Transib', 'Mariner', 'Zator', 'L1', 'RTE-X', '5S-RNA_Promoter', 'Group-I', 'Hero', 'scRNA', 'Poseidon', 
+    'MuDR', 'Mutator-like', 'ISL2EU', 'Tad1_End', 'PiggyBac-X', 'DNA_Polymerase', 'Hydra-specific_Branch', 
+    'Academ-2', 'Pararetroviridae', 'CR1-end', 'Odin', 'P_Element', 'tRNA_and_7SL_RNA', 'Stowaway', 'L1-group', 
+    'Fot1', 'Restless', 'Retrotransposon', 'Casposon', 'CRE-1', 'Crypton-I', 'Orthoretrovirinae', 'Tc4', 
+    'Unknown_LINE-dependent', 'Hydra', 'Charlie', 'ERV2-group', 'Viper-group', 'R4-like', 'Tc1-Mariner', 
+    'RTE-end', 'R1-end', 'R1-subgroup', 'DIRS', 'Kolobok', 'R1', 'ISRm11', 'Gypsy-ERV', 'Crypton-R', 'Zorro', 
+    'RTE-group', 'Chapaev_group', 'Ty1-Copia', 'Academ-1', 'Sola', 'Tyrosine_Recombinase', 'Q', 'Rex-end', 
+    'Tyrosine_Recombinase_Elements', 'HarbS', 'Long_Terminal_Repeat_Element', 'Crypton-S', 'R1-group', 
+    'Zisupton', 'LINE', 'hobo', 'BovB-end', 'Pseudogene', 'Mermaid', 'Penelope-like_Elements', 'snRNA', 
+    'Tad1', 'Ginger-2', 'ERV2', 'IS3EU', 'CMC', 'SINE-like', 'ERV1', 'T2', 'Novosib', 'Transposable_Element', 
+    'Crypton-V', 'Dong-R4', 'Deceiver', 'ERV3', 'R2', 'Alu', 'L2-derived', 'L1-derived', 'SINE', 'ERV4', 'Ant1', 
+    'V_and_MIR-core', 'Group-II', 'Rex-Babar', 'Ceph-core', 'Sola-3', 'Class_II_DNA_Transposition', 'Crypton', 
+    'Neptune', 'Helicase', 'CACTA', 'CRE-2', 'CR1', 'Coprina', 'Athena', 'Blackjack', 'TRIM', 'm44', 'Mirage', 
+    'Caulimoviridae', 'Academ-H', 'LOA', 'NOF', 'Tx1', 'CR1-group', 'I-end', 'Bel-Pao', 'Maverick', 'Nematis', 
+    'Transposase', 'Crypton-A', 'ORTE', 'Ginger', 'Tag1', 'R2-like', 'Merlin', 'PiggyBac', 'Kolobok-H', 
+    'I-group', 'Dada', 'Tc2', 'Tc2-group', 'Pegasus', 'Gypsy', 'tRNA', 'hATx', 'Class_I_Retrotransposition', 
+    'Ambal', 'Ricksha', 'Jockey', 'RTE', 'BovB', 'Chlamys', 'Meta-core', 'Proto-1', 'hATw', 'RNA', 'hAT1', 
+    'L1-dependent', 'Genie', 'Lenti', 'I', 'L2-group', 'Sauria-core', 'EnSpm', 'Harbinger', 'No-core', 
+    'Lacking_Small_RNA_pol_III_Promoter', 'SVA', 'I-derived', 'CRE', 'Tigger', 'Proto-2', 'Cweed', 'DRE', 
+    'Sola-1', 'R2-end', 'hATm', 'R4-derived', 'Helitron-1', 'Crypton-C', 'Unknown_Promoter', 
+    'Interspersed_Repeat', 'Crypton-F', 'Crypton-H', 'RTE-derived', 'No_or_Unknown_Core', 'L2', 
+    'Centromeric', 'L2-end', 'Group-2', 'RTE-like', 'PiggyBac_Group', 'Chapaev', 'Fungi-specific_Branch', 
+    'MaLR', 'IS885', 'V-core', 'Gizmo', 'Unknown', 'Tip100', 'Spy', 'Academ', 'Activator', 'Jockey-end', 
+    'tRNA_and_5S_RNA', 'hAT5', 'TATE', 'Tc1', 'Zenon', 'F', 'Mogwai', '7SL-RNA_Promoter', 'PIF-Harbinger', 
+    'snoRNA', 'Chapaev-3', 'L1-like', 'Helitron-2', 'Ginger-1', 'Kolobok-E', 'hAT19', 'B2', 'rRNA', 
+    'U-RNA_Promoter', 'Dualen', 'Maverick-Mavirus', 'Crypton-X', 'NeSL', 'Ngaro', 'Sagan', 'MIR-core', 
+    'Sola-2', 'Retroviridae', 'Naiad', 'hAT', 'PiggyBac-A', 'LINE-dependent_Retroposon', 'Pogo', 'hAT6', 
+    'tRNA_Promoter', 'R1-like', 'Viper', 'Group-1', 'Spumaretrovirinae', 'Deu-core'}
 
 class Data:
 
@@ -30,23 +62,26 @@ class Data:
 
         saida: pd.DataFrame = pd.DataFrame(resultados)[['accession', 'classification', 'consensus_sequence']]
 
+        saida['classification'] = saida['classification'].apply(self.lista_binaria)
+
         self.params["start"] += self.taxa_dowload
         
         return saida
+    
+    def lista_binaria(self, classificacao_H: str) -> np.ndarray:
+        
+        classificacao_H = classificacao_H.split(';')
+
+        return np.array((1 if i in todas_hierarquias else 0 for i in classificacao_H))
+    
+    def conserved_domain:
 
 
 def main() -> None:
 
-    a = time.time()
+    data = Data(5)
 
-    data = Data(1000)
-    # data = Data(5)
-
-    data.get_data().to_csv("C:\\Users\\UsuarioOct\\Documentos\\trabalhos\\Em produção\\IC\\code\\temp_data\\dado.csv")
-
-    b = time.time()
-
-    print(b-a)
+    print(data.get_data().head())
 
     return None
 
--- classes/testes.py
+++ classes/testes.py
@@ -1,25 +1,26 @@
-import requests
+import pandas as pd
+import numpy as np
 
-# Endpoint da API para detalhes de uma família específica
-url = 'https://dfam.org/api/families/DF000001643/assemblies'
-url = 'https://dfam.org/api/classes'
-url = 'https://dfam.org/api/version'
-url = 'https://dfam.org/api/families/DF000000012'
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
 
+# Sua sequência de exemplo
+sequence = "TCACCCCCAGCCCTCAGGGACTCTCCTGTCTGTCCCAGCTACTCTCCAACCACGCCCAGATTTCAGCNGGAGTCAGTTCCAGGCACCCAGGAATCACCNNCAAACTCACCGATTTCACTGAGTTACTCCCCAGTCTCTCTCACGTCT"
 
+# Realizar uma busca de domínios conservados usando BLAST no NCBI
+result_handle = NCBIWWW.qblast("blastn", "nt", sequence)
 
+# Parsear os resultados do BLAST
+blast_records = NCBIXML.read(result_handle)
 
-response = requests.get(url)
+# Exibir resultados
+for alignment in blast_records.alignments:
+    for hsp in alignment.hsps:
+        print("****Alignment****")
+        print("sequence:", alignment.title)
+        print("length:", alignment.length)
+        print("e value:", hsp.expect)
+        print(hsp.query[0:75] + "...")
+        print(hsp.match[0:75] + "...")
+        print(hsp.sbjct[0:75] + "...")
 
-# Verificando se a requisição foi bem-sucedida
-if response.status_code == 200:
-
-    try:
-        data = response.json()
-        print(data)
-
-    except:
-
-        print(response.text)
-else:
-    print('Erro:', response.status_code)
