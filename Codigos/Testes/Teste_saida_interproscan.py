331e68b9c4ada968b40663e7ee194224ae94e997
LucasMantvani <joaolucasmbaioco@gmail.com>
2024-08-01 13:37:35 -0300

    #

--- Codigos/Testes/Teste_saida_interproscan.py
+++ Codigos/Testes/Teste_saida_interproscan.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 file_path = '/home/mantovani/Documents/trabalhos/Em_produção/IC/HMC_TE-s/Temp_Data/Dados_Dominos_Consevados/sequences.json'
 
@@ -6,10 +7,19 @@ with open(file_path, 'r') as file:
 
     dados = json.load(file)
 
-    gene_1 = dados['results'][0]['openReadingFrames']
+    for i in dados['results']:
 
-    for i in gene_1:
+        gene_1 = i['openReadingFrames']
 
-        print(i['protein']['matches'], '\n')
+        df = pd.DataFrame([{
+        'id_DC': item['id'],
+        'start': item['start'],
+        'end': item['end'],
+        'strand': item['strand'],
+        'protein_sequence': item['protein']['sequence'],
+        } for item in gene_1])
+
+            # print(j['protein']['matches'], '\n')
+        print(df, '\n')
 
     file.close
